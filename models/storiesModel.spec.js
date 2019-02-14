713cb55e2a346fdd14b7a9bb98693f2a81635ad8
Julie Jonak <juliejonak10@gmail.com>
2019-02-13 22:11:01 -0600

    Tests outlined: - storiesModel.spec.js: Needed model functions outlined - usersModel.spec.js: Tests consider how to return data more easily for front end

--- models/storiesModel.spec.js
+++ models/storiesModel.spec.js
@@ -2,3 +2,34 @@ const stories = require('./storiesModel');
 const db = require('../data/dbConfig');
 
 //test helper functions of get stories, get story by id, get stories by user id, post story, put story, delete story
+
+describe('the stories model', () => {
+
+    it('should fetch all the stories', async () =>{
+        //return array of stories
+        //fetch()
+    })
+
+    it('should fetch a story by id', async () =>{
+        //return single story
+        //fetch(id)
+    })
+
+    it('should fetch stories by user id', async () =>{
+        //fetchUserStories(userid)
+        //return array of stories by that user
+    })
+
+    it('should add a new story', async () =>{
+        //add(newStory)
+    })
+
+    it('should update a story', async () =>{
+        //update(updatedStory)
+    })
+
+    it('should delete a story', async () =>{
+        //remove(id)
+        //expect(deleted).toBe(1)
+    })
+})
--- models/usersModel.spec.js
+++ models/usersModel.spec.js
@@ -1,5 +1,34 @@
 const users = require('./usersModel');
 const db = require('../data/dbConfig');
 
-//test helper function for get user by id, get user by username, post user, put user, delete user
+//test helper function for get user by id, get user by username, post user, put user, delete user, get user info for story post
 
+describe('the users model', () => {
+
+    it('should get user by id', async () => {
+        //fetch(id), return object
+    })
+
+    it('should get user country for a new post', async () => {
+        //fetchCountry
+        //only return country based on id
+    })
+
+    it('should post a new user', async () => {
+        //register, return new id
+    })
+
+    it('should find a user by username to login', async () => {
+        //login(username)
+        //return only username and password
+    })
+
+    it(`should update a user's information`, async () => {
+        //update(id)
+    })
+
+    it('should delete a user', async () => {
+        //remove(id)
+    })
+
+})
