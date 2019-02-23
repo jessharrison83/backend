e046332cd899b46823db2a1929066e7a99c02373
Jess <jessdev83@gmail.com>
2019-02-23 14:10:42 -0500

    index.js changes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const {server} = require('./api/server');
+const { server } = require("./api/server.js");
 
 const port = process.env.PORT || 3100;
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
 server.listen(port, () => {
-    console.log(`Server is listening on ${port}.`)
-})
+  console.log(`Server is listening on ${port}.`);
+});
