85460bd87c94054f0a137cb89b284fe011c266b5
Jess <jessdev83@gmail.com>
2019-02-23 14:12:10 -0500

    index changes

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const { server } = require("./api/server.js");
 
 const port = process.env.PORT || 3100;
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-client.connect();
-
 server.listen(port, () => {
   console.log(`Server is listening on ${port}.`);
 });
