57aec9cd5ee79a64d652c9d69315d5ca947e7435
Jess <jessdev83@gmail.com>
2019-02-23 14:16:10 -0500

    index changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { server } = require("./api/server.js");
+const server = require("./api/server.js");
 
 const port = process.env.PORT || 3100;
 const { Client } = require("pg");
