9d9c749bdcbbb7d01f7224e3652d93bdb3735dd5
Jess <jessdev83@gmail.com>
2019-02-23 14:07:36 -0500

    changed char length

--- data/migrations/20190212223047_stories.js
+++ data/migrations/20190212223047_stories.js
@@ -1,17 +1,23 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('stories', table => {
-      table.increments();
-      table.string('title', 250).notNullable();
-      table.string('country').notNullable();
-      table.string('description').notNullable();
-      table.string('updated_at').notNullable().defaultTo(knex.fn.now());
-      table.string('small_image').notNullable();
-      table.string('large_image').notNullable();
-      table.integer('user_id').unsigned().references('id').inTable('users');
-  })
+  return knex.schema.createTable("stories", table => {
+    table.increments();
+    table.string("title", 250).notNullable();
+    table.string("country").notNullable();
+    table.string("description", 100000).notNullable();
+    table
+      .string("updated_at")
+      .notNullable()
+      .defaultTo(knex.fn.now());
+    table.string("small_image").notNullable();
+    table.string("large_image").notNullable();
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('stories')
+  return knex.schema.dropTableIfExists("stories");
 };
